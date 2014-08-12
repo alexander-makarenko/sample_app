ccb4f2ae6e82f262c979c7cfe34c0c0cbf91d7bd
Alexander Makarenko <alexander.makarenko@zoho.com>
2014-08-12 17:38:46 +0300

    Add a Contact page and a page title helper

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 feature "Static pages" do
   
+  given(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
   feature "Home page" do
     background { visit '/static_pages/home' }
-  
+    
     scenario "should have the content 'Sample App'" do
       expect(page).to have_content('Sample App')
     end
 
-    scenario "should have the right title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    scenario "should have the base" do
+      expect(page).to have_title("#{base_title}")
+    end
+
+    scenario "should not have a custom page title" do
+      expect(page).not_to have_title("| Home")
     end
   end
 
@@ -22,7 +28,7 @@ feature "Static pages" do
     end
 
     scenario "should have the right title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -34,7 +40,19 @@ feature "Static pages" do
     end
 
     scenario "should have the right title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
+    end
+  end
+
+  feature "Contact page" do
+    background { visit '/static_pages/contact' }
+  
+    scenario "should have the content 'Contact'" do
+      expect(page).to have_content('Contact')
+    end
+
+    scenario "should have the right title" do
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
